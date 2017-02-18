2ba51a4afaa6c69d6ff8f8164f57601ecff3bcfd
daveaugerinos <dave.augerinos@gmail.com>
2017-02-18 11:53:15 -0800

    Tab Bar Controller initial setup

---
+++ iOS-Gestures/iOS-Gestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS-Gestures
+//
+//  Created by Dave Augerinos on 2017-02-18.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
